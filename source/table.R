a696b605e6ce845eaaaf06cdc43e95c07ec6522f
= <=>
2022-11-14 14:08:16 -0800

    rendered rough table

--- source/table.R
+++ source/table.R
@@ -5,7 +5,13 @@ gun_deaths <- read.csv("../data/US_gun_deaths_1985-2018.csv")
 #data2 <- data1[-c(1,2),]
 #View(data2)
 
-table_plz <- gun_deaths %>% 
-  group_by(year) %>% 
-  filter(!is.na(as.numeric(victim_age)))
+# table_plz <- gun_deaths %>% 
+#   group_by(year) %>% 
+#   filter(!is.na(as.numeric(victim_age)))
   # summarise(avg_victim_age = mean(victim_age, na.rm = TRUE))
+
+n_weapons <- gun_deaths %>% 
+  group_by(weapon_used) %>% 
+  summarise(counts = n()) %>% 
+  return(n_weapons)
+View(n_weapons)
