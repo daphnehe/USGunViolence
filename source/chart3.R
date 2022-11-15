7b560793b06ccd14ad49cd68f4a6a33cac43dacd
Cecelia <cecelia@thomasohana.com>
2022-11-14 18:47:28 -0800

    chart 3 done

--- source/chart3.R
+++ source/chart3.R
@@ -15,7 +15,7 @@ colnames(shootings_by_state)[2] = "shootings_2013_2022"
 
 chart3 <- shootings_by_state %>%
   ggplot(aes(x = state, y = shootings_2013_2022, fill = shootings_2013_2022)) +
-  scale_fill_gradient(low = "gray", high = muted("purple", l = 40, c = 60)) +
+  scale_fill_gradient(low = "gray", high = "purple") +
   geom_col() +
   theme(axis.text.x = element_text(size = 5, angle = 45, hjust = 1))
 plot(chart3)
