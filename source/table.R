2988d43eb3dfddb1294545a474313b1465419849
= <=>
2022-11-14 20:36:01 -0800

    table

--- source/table.R
+++ source/table.R
@@ -1,27 +1,24 @@
 #install.packages("ggplot2")
 library(ggplot2)
 library(tidyverse)
-gun_deaths <- read.csv("../data/US_gun_deaths_1985-2018.csv")
+mass_shootings <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-CeceliaKT/main/data/Mass%20Shootings%20Dataset%20Ver%205.csv")
 #data2 <- data1[-c(1,2),]
-#View(data2)
+View(mass_shootings)
 
 # table_plz <- gun_deaths %>% 
 #   group_by(year) %>% 
 #   filter(!is.na(as.numeric(victim_age)))
-  # summarise(avg_victim_age = mean(victim_age, na.rm = TRUE))
+# summarise(avg_victim_age = mean(victim_age, na.rm = TRUE))
 
-<<<<<<< HEAD
-n_weapons <- gun_deaths %>% 
-  group_by(weapon_used) %>% 
-  summarise(counts = n()) %>% 
-  return(n_weapons)
-View(n_weapons)
-=======
+
 #n_weapons <- gun_deaths %>% 
-  #group_by(weapon_used) %>% 
-  #summarise(counts = n()) %>% 
-  #return(n_weapons)
+#group_by(weapon_used) %>% 
+#summarise(counts = n()) %>% 
+#return(n_weapons)
 #View(n_weapons)
 
->>>>>>> 6631812c2bb8175841cfc018b1367e8a7099c826
 
+gender <- mass_shootings %>%
+  group_by(Gender) %>%
+  summarize(counts = n())
+View(gender)
