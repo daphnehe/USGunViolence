4ec9bfe02b67d31bbeb967dd8f2e88457b6ef783
stephliu39 <sliu39@uw.edu>
2022-12-07 15:06:55 -0800

    added css and html

--- source/final_deliverable/tabs/tab_panel_report.R
+++ source/final_deliverable/tabs/tab_panel_report.R
@@ -4,27 +4,11 @@ library(shiny)
 library("markdown")
 
 tab_panel_report <-tabPanel(
-<<<<<<< HEAD
-  h5("Report", style = "color: #AFA1CB"),
 
-  fluidRow(
-    column(
-      6,
-      includeMarkdown("tabs/report.md")
-    ),
-    column(
-      3,
-      p("This is a paragraph.")
-    ),
-    column(
-      3,
-      p("Rightmost column.")
-    )
-  )
-=======
-  "Report",
+
+  h5("Report", style = "color: #AFA1CB"),
   includeMarkdown("tabs/report.md")
->>>>>>> 7e5f1645b5add527a7a4c3f7d3f0dd180d4d8e4f
+
 
 
 )
