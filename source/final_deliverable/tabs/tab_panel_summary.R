4e5ca420a57149a915a2f53281700a6cf418b892
= <=>
2022-12-08 02:39:35 -0800

    done done

--- source/final_deliverable/tabs/tab_panel_summary.R
+++ source/final_deliverable/tabs/tab_panel_summary.R
@@ -9,8 +9,7 @@ tab_panel_summary <-tabPanel(
       California, generally had lower gun deaths per capita as compared to 
       states with lower rates of gun control, such as Kansas. 2) We also found 
       that handguns have the highest rate of usage in gun violence crimes, 
-      which implicates that restrictions should be applied to all kinds of 
-      weapons, not just a select few. 3) Finally, we also found that Black people 
+      which implicates that restrictions like handgun waiting periods would be effective in reducing gun deaths in the US. 3) Finally, we also found that Black people 
       are more likely to be killed by guns. Black people make up only 13% of the 
       US population, but make up over half of gun deaths in the US. This shows 
       how society has failed to protect Black communities from gun violence,
